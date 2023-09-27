1a596492bc35d383401da78c843234cf0e8c9fb6
mmarentette <melrosemarentette@gmail.com>
2023-09-27 15:55:47 -0400

    Learned about the return statement

--- js/app.js
+++ js/app.js
@@ -30,3 +30,29 @@ sayGoodbye('Adios', 'Laura');
 sayGoodbye('Ciao', 'Becky');
 sayGoodbye('Goodbye', 'Chad');
 sayGoodbye('Peace', 'Frankie');
+
+function add(x, y) {
+    return x + y; // return produces a value for the function when it is called (but not all functions need to return a value)
+}
+
+// Example: Video Game
+function getPlayerUserName(username) {
+    return username;
+}
+
+// Before a game, we don't know the players' names
+let playerOne;
+let playerTwo;
+let playerThree;
+let playerFour;
+
+// After they join the game, we set the player's name
+// We want these lines of code below to produce a value, so we use return instead of console.log()
+playerOne = getPlayerUserName('Luke'); // == > 'Luke' 
+playerTwo = getPlayerUserName('Laura'); // == > 'Laura'
+playerThree = getPlayerUserName('kate'); // == > 'kate'
+playerFour = getPlayerUserName('Kevin'); // == > 'Kevin'
+
+// James' best advice for the course: No matter what you think some variable is, you do not know the value is unless you console.log() it. This is your BEST TOOL.
+// We use return when we want our functions to produce some value. Sometimes we return things; sometimes we don't.
+// Don't mix up printing to the console and returning! If you use console.log() only, you can print something, but you won't return anything!
