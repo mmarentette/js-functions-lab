8a63c008b65bec0ca7368aa88baab8cdadc0f299
mmarentette <melrosemarentette@gmail.com>
2023-09-27 15:30:14 -0400

    Learning how functions work

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 console.log('app.js is loading')
 
-//testing
+// defining a function
+// declaring a function
+// creating the reusable block of code to be used at a later time in the program
+function sayHello(personsName) {
+    // write the code to say hello here
+    console.log(`Hello, ${personsName}`)
+}
+
+// execute the function
+// invoke the function
+// call the function
+
+// write the function name ()
+sayHello('Shavon'); // runs the block of code defined by the sayHello function
+sayHello('Megan');
+sayHello('Ed');
+sayHello('Augusta');
+
+
+// Define and call the function
+// sayGoodbye should accept two parameters, one phrase, and one person's name
+
+function sayGoodbye(phrase, personsName) {
+    console.log(`${phrase}, ${personsName}`)
+}
+
+sayGoodbye('Adios', 'Laura');
+sayGoodbye('Ciao', 'Becky');
+sayGoodbye('Goodbye', 'Chad');
+sayGoodbye('Peace', 'Frankie');
